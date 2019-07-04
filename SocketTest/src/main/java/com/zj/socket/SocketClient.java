bca8fd4e8ef83c1858fc56778c55a648e2836f14
ZJ0703 <you@example.com>
2019-07-04 18:26:00 +0800

    first one

---
+++ SocketTest/src/main/java/com/zj/socket/SocketClient.java
@@ -0,0 +1,31 @@
+package com.zj.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		
+		Socket socket = new Socket(InetAddress.getLocalHost(),4015);
+		System.err.println("client");
+		
+		//socket.getOutputStream().write(2);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(socket.getOutputStream());
+		BufferedReader readStringIn = new BufferedReader(new InputStreamReader(System.in));//读取控制台数据
+		while(true) {
+			
+			out.println(readStringIn.readLine());
+			out.flush();
+		System.err.println(in.readLine());
+		
+		}
+		
+	}
+}
---
+++ SocketTest/src/main/java/com/zj/socket/SocketServer.java
@@ -0,0 +1,30 @@
+package com.zj.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(4015);
+		System.err.println("server");
+		System.err.println(1);
+		Socket client = server.accept();//产生socket连接
+		while(true) {
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			PrintWriter out = new PrintWriter(client.getOutputStream());
+			/*out.write("Hello");
+			out.flush();*/
+			System.err.println(2);
+			System.out.println(in.readLine());//这里读取数据读不到阻塞
+			out.println("end");
+			out.flush();
+		}
+	}
+}
